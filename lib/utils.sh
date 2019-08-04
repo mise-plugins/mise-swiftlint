a269cdc59277a8920ec5d0ea723399c12a9bcc54
Kevin Lundberg <kevin@klundberg.com>
2019-08-04 00:23:11 -0400

    Fixing list-all to work properly during testing

--- lib/utils.sh
+++ lib/utils.sh
@@ -6,13 +6,21 @@ echoerr() {
   >&2 echo -e "\033[0;31m$1\033[0m"
 }
 
+utils_path() {
+    echo "$(dirname "$(dirname "$0")")/utils"
+}
+
 jq_path() {
-    echo "$(dirname "$(dirname "$0")")/utils/jq"
+    echo "$(utils_path)/jq"
 }
 
 download_jq_if_needed() {
-    if [ ! -f "$(jq_path)" ]; then
-        curl $JQ_URL -L -o "$(jq_path)"
-        chmod +x "$(jq_path)"
+    local JQ_PATH
+    JQ_PATH="$(jq_path)"
+
+    if [ ! -f "$JQ_PATH" ]; then
+        mkdir -p "$(utils_path)"
+        curl "$JQ_URL" -L -o "$JQ_PATH" 2>/dev/null
+        chmod u+x "$JQ_PATH"
     fi
 }
